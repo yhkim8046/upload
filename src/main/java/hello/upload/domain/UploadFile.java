549d6df10439b05c2445f6a113d277b802172bc3
Yuhwan Kim <yhkim8046@gmail.com>
2024-03-16 00:56:51 +1300

    Uploading and saving done

--- src/main/java/hello/upload/domain/UploadFile.java
+++ src/main/java/hello/upload/domain/UploadFile.java
@@ -8,5 +8,8 @@ public class UploadFile {
     private String uploadFileName;
     private String storeFileName;
 
-
+    public UploadFile(String uploadFileName, String storeFileName) {
+        this.uploadFileName = uploadFileName;
+        this.storeFileName = storeFileName;
+    }
 }
